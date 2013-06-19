b4056169157f35cee553312321b5867660fc3690
parkjh <jpark804@gmail.com>
2013-06-19 20:03:15 +0100

    test

---
+++ azplugins/aizen/AizenPlugin.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package azplugins.aizen;
+
+import org.gudy.azureus2.plugins.Plugin;
+import org.gudy.azureus2.plugins.PluginException;
+import org.gudy.azureus2.plugins.PluginInterface;
+
+/**
+ * @author joon
+ *
+ */
+public class AizenPlugin implements Plugin {
+
+	/* (non-Javadoc)
+	 * @see org.gudy.azureus2.plugins.Plugin#initialize(org.gudy.azureus2.plugins.PluginInterface)
+	 */
+	@Override
+	public void initialize(PluginInterface pluginInterface)
+			throws PluginException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
